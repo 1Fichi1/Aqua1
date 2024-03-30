ff4249b637579e7b60df05c7212f4061b61daf57
1Fichi1 <anderpen2006time@gmail.com>
2024-03-30 07:17:18 +0300

    наконец-то

---
+++ AquaWPF2/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AquaWPF2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        private AquaEntities Customers = new AquaEntities();
+        public Page1()
+        {
+            InitializeComponent();
+            Aqua1.ItemsSource = Customers.Customers.ToList();
+        }
+    }
+}
---
+++ AquaWPF2/Page3.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AquaWPF2
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        private AquaEntities Products = new AquaEntities();
+        public Page3()
+        {
+            InitializeComponent();
+            Aqua3.ItemsSource = Products.Products.ToList();
+        }
+    }
+}
